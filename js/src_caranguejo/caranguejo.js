e6a68ec62ae2a9252c028defedcd7fed150c7899
SaltaFora <pctrolp@gmail.com>
2022-02-13 16:33:31 +0000

    js começo

--- js/src_caranguejo/caranguejo.js
+++ js/src_caranguejo/caranguejo.js
@@ -1,17 +1,36 @@
 const caranguejo = document.getElementById("caranguejo")
 var bottom=0
 
+
+
+
 function saltar(){
-bottom +=30
-    caranguejo.style.bottom = bottom + "px"
+    var timerup = setInterval(function (){
+        if (bottom>250){
+            clearInterval(timerup);
+            var timerdown =setInterval(function (){
+                if (bottom === 0){
+                    clearInterval(timerdown)
+                }
+                bottom -=20
+                caranguejo.style.bottom = bottom + "px";
+            }, 20)
+        }
+        bottom +=30;
+        caranguejo.style.bottom = bottom + "px";}, 20)
 }
+
+
 
 window.onkeydown = function(event) {
     processa_tecla(event.key)
 }
 
 function processa_tecla(key) {
-    if (key == " ") {
+    if (key === " ") {
+        saltar();
+    }
+    else if (key === "ArrowUp"){
         saltar();
     }
 }
