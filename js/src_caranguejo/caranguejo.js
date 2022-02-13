0a8800d441f4ba466c002fd49d52ab8fc0d5900b
SaltaFora <pctrolp@gmail.com>
2022-02-13 19:46:58 +0000

    js começo

--- js/src_caranguejo/caranguejo.js
+++ js/src_caranguejo/caranguejo.js
@@ -1,26 +1,62 @@
 const caranguejo = document.getElementById("caranguejo")
 var bottom=0
-
-
-
+var left=0
+var gravity=0.9
+var air=false // caranguejo está a saltar
+var andarparaesquerda = false
+var andarparadireita = false
+var timerdireita
+var timeresquerda
 
 function saltar(){
+    if (air) return // impedir double jump
     var timerup = setInterval(function (){
         if (bottom>250){
             clearInterval(timerup);
             var timerdown =setInterval(function (){
-                if (bottom === 0){
+                if (bottom <0){
                     clearInterval(timerdown)
+                    air=false
                 }
-                bottom -=20
+                bottom -=5
                 caranguejo.style.bottom = bottom + "px";
             }, 20)
         }
+
+        air= true
         bottom +=30;
+        bottom= bottom * gravity
         caranguejo.style.bottom = bottom + "px";}, 20)
 }
 
+function esquerda() {
+    document.getElementById("imagem").src="../imagens/andar%20esquerda.png";
+    if (andarparadireita){
+        clearInterval(timerdireita);
+        andarparadireita= false;
+    }
+    andarparaesquerda = true
+     timeresquerda = setInterval(function () {
+         console.log("esquerda")
+        left -= 5;
+        caranguejo.style.left = left + "px";
+    }, 20)
+}
 
+    function direita(){
+        document.getElementById("imagem").src="../imagens/andar%20direita.png";
+    if (andarparaesquerda){
+        clearInterval(timeresquerda);
+        andarparaesquerda=false;
+    }
+        andarparadireita=true
+         timerdireita = setInterval(function (){
+             console.log("direita")
+            left += 5;
+            caranguejo.style.left = left + "px";
+        },20)
+
+}
 
 window.onkeydown = function(event) {
     processa_tecla(event.key)
@@ -33,4 +69,11 @@ function processa_tecla(key) {
     else if (key === "ArrowUp"){
         saltar();
     }
+    else if (key === "ArrowLeft"){
+        esquerda();
+    }
+    else if (key === "ArrowRight"){
+        direita();
+    }
+
 }
