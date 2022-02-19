22387e4de5fcb26629fbd5ef358b5c8e52643037
Francisco Abreu <franciscoabreu650@gmail.com>
2022-02-19 15:13:07 +0000

    cenários fundo

--- SaltaFora/Sapo.js
+++ SaltaFora/Sapo.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let keysDown = {}
 
-    const grid=document.querySelector(".grid");
+    const grid=document.querySelector(".gridSapo");
     const sapo = document.createElement("div");
     let isGameOver = false;
     let platformCount = 13;
